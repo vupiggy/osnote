cc480dc8bccb0c81c0f5098a83795260ccab6d71
Luke Huang <lukehuang.ca@gmail.com>
2019-12-18 08:09:36 -0500

    fixed some over and under fulls

--- src/container_of.c
+++ src/container_of.c
@@ -1,13 +1,9 @@
-/**
- * container_of - cast a member of a structure out to the containing structure
+/* container_of - cast a member of a structure out to the containing structure
  * @ptr:	the pointer to the member.
  * @type:	the type of the container struct this is embedded in.
  * @member:	the name of the member within the struct.
- *
  */
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)−>MEMBER)
 #define container_of(ptr, type, member) ({				\
       const typeof( ((type *)0)->member ) *__mptr = (ptr);		\
       (type *)( (char *)__mptr - offsetof(type,member) );})
-
-
